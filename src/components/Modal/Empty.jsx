0df2f90e351cc389b853a7aac5cb6c2a20256e01
Daniel Marin <danielmarin@MacBook-Air-de-SitDigital.local>
2022-06-24 16:58:29 -0500

    validacion formularios

--- src/components/Modal/Empty.jsx
+++ src/components/Modal/Empty.jsx
@@ -1,9 +1,10 @@
 import React,{useState} from 'react';
-import { Modal, Button, Container, Row, Col } from 'react-bootstrap';
+import { Modal, Button, Container, Row, Col , Alert} from 'react-bootstrap';
 import {updateEmployee,formEmployee, dataForm} from '../../actions';
 import { useDispatch, useSelector } from 'react-redux';
 const Moda = ({title, children, titleB, action}) => {
     const [show, setShow] = useState(false);
+    const [alert,setAlert] = useState(false)
     const dispatch = useDispatch();
     const form = useSelector(state => state.formEmployee)
     const handleClose = () => {
@@ -12,12 +13,29 @@ const Moda = ({title, children, titleB, action}) => {
     };
     const handleSubmit = () => {
       console.log("entre al correcto")
+      console.log(form)
+      console.log("gato")
+      let isEmpty=false;
+      if(form){
+      Object.keys(form).forEach(prop=>{
+        if(form[prop]=='') isEmpty=true
+        else if(prop!='filters'&&prop!='userLikes'&&prop!='userDislikes')
+        isEmpty=false
+        else if(form.date==='') isEmpty=true
+      })
+      if(!isEmpty){
       form&&dispatch(action.create(form))
       setTimeout(function(){
         form&&dispatch(action.refresh())
         dispatch(formEmployee(null))
+        setAlert(false)
       setShow(false)
          }, 1000);
+      }else{
+        
+       setAlert(true)
+      }
+    }
   };
     const handleShow = () => {
         setShow(true)
@@ -35,6 +53,9 @@ const Moda = ({title, children, titleB, action}) => {
           </Modal.Header>
           <Modal.Body className="show-grid">
         <Container>
+          {alert&&<Alert key={'warning'} variant={'warning'}>
+      Los campos no pueden estar vacios!
+    </Alert>}
           <Row>
             <Col xs={12} md={12}>
             {children}
